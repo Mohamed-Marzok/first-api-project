c5ddc28ab6c3577bcf0f378dcfd72c8cba14f8cd
Mohamed-Marzok <142693140+Mohamed-Marzok@users.noreply.github.com>
2024-02-10 13:38:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,41 @@
 let container = document.querySelector(".container");
 let main = document.querySelector(".main");
 let sideBar = document.querySelector(".side-bar");
-let makeRequest = function (type, url) {
-  let req = new XMLHttpRequest();
-  req.open(type, url);
-  req.send();
-  return req;
+
+const getAllUser = function () {
+  return fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((user) => {
+        let userHtml = `<div class='user' onclick="userActive(${user.id}, this)">${user.name}</div>`;
+        sideBar.insertAdjacentHTML("beforeend", userHtml);
+      });
+    });
 };
-let makeUserSet = function (data) {
-  let userSet = new Set([]);
-  data.forEach((element) => {
-    userSet.add(element.userId);
-  });
-  return userSet;
+const getUserPost = function (userId) {
+  main.innerHTML = "";
+  return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((post) => {
+        let postHtml = `
+        <div class='post'>
+          <div class='post-title'>${post.title}</div>
+          <div class='post-body'>${post.body}</div>
+        </div>`;
+        main.insertAdjacentHTML("beforeend", postHtml);
+      });
+    });
 };
-const giveActiveClass = function (users) {
+getAllUser();
+
+function userActive(userId, user) {
+  getUserPost(userId);
+  let users = document.getElementsByClassName("user");
   [...users].forEach((user) => {
-    user.addEventListener("click", (e) => {
-      main.innerHTML = "";
-      [...users].forEach((user) => {
-        user.classList.remove("active");
-      });
-      user.classList.add("active");
-      let activeUser = document.querySelector(".active");
-      console.log(activeUser.innerHTML);
-      let req = makeRequest(
-        "GET",
-        `https://jsonplaceholder.typicode.com/posts?userId=${activeUser.innerHTML}`
-      );
-      req.addEventListener("load", function () {
-        console.log(JSON.parse(req.responseText));
-        let data = JSON.parse(req.responseText);
-        data.forEach((post) => {
-          let html = `
-          <div class='post'>
-            <div class='post-title'>${post.title}</div>
-            <div class='post-body'>${post.body}</div>
-          </div>`;
-          main.insertAdjacentHTML("beforeend", html);
-          console.log(post.id);
-        });
-      });
-    });
+    user.classList.remove("active");
   });
-};
-let readAllPost = function () {
-  let req = makeRequest("GET", "https://jsonplaceholder.typicode.com/posts");
-  req.addEventListener("load", function () {
-    console.log(JSON.parse(req.responseText));
-    let data = JSON.parse(req.responseText);
-    let userSet = makeUserSet(data);
-    userSet.forEach((user) => {
-      let userHtml = `<div class='user'>${user}</div>`;
-      sideBar.insertAdjacentHTML("beforeend", userHtml);
-    });
-    let users = document.querySelectorAll(".user");
-    giveActiveClass(users);
-  });
-};
-readAllPost();
+  user.classList.add("active");
+}
